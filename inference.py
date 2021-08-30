97205cdffb1f24de0e0a06ee99b437d8df9ef8b2
lkm2835 <lkm2835@gmail.com>
2021-08-30 17:45:04 +0900

    vanila baseline code

--- inference.py
+++ inference.py
@@ -1,81 +1,89 @@
-import os
-import pandas as pd
-from PIL import Image
-
-import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from torchvision.transforms import Resize, ToTensor, Normalize
-
 import argparse
+import os
+from importlib import import_module
 
-from model.models import *
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
 
-class TestDataset(Dataset):
-    def __init__(self, img_paths, transform):
-        self.img_paths = img_paths
-        self.transform = transform
+from dataset import TestDataset, MaskBaseDataset
 
-    def __getitem__(self, index):
-        image = Image.open(self.img_paths[index])
 
-        if self.transform:
-            image = self.transform(image)
-        return image
-
-    def __len__(self):
-        return len(self.img_paths)
-
-if __name__ == '__main__':
-    args = argparse.ArgumentParser(description='inference_path')
-    args.add_argument('-i', '--image_dir', default='./eval', type=str, help='eval image folder path')
-    args.add_argument('-l', '--load_state', default='./saved/model.pt', type=str, help='load state file path (Default ./saved/model.pt)')
-    args.add_argument('-m', '--model', default='PretrainedResnet', type=str, help='choose model')
-    args = args.parse_args()
-
-    # meta 데이터와 이미지 경로를 불러옵니다.
-    test_dir = args.image_dir
-    load_file_path = args.load_state
-    model_name = args.model
-    
-    print(ModelList.parse_model(model_name)(xdim=[3,512,384], ksize=3, cdims=[], hdims=[256], ydim=18))
-
-    submission = pd.read_csv(os.path.join(test_dir, 'info.csv'))
-    image_dir = os.path.join(test_dir, 'images')
-
-    # Test Dataset 클래스 객체를 생성하고 DataLoader를 만듭니다.
-    image_paths = [os.path.join(image_dir, img_id) for img_id in submission.ImageID]
-    transform = transforms.Compose([
-        Resize((512, 384), Image.BILINEAR),
-        #transforms.CenterCrop((300)),
-        ToTensor(),
-        Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
-    ])
-    dataset = TestDataset(image_paths, transform)
-
-    loader = DataLoader(
-        dataset,
-        shuffle=False
+def load_model(saved_model, num_classes, device):
+    model_cls = getattr(import_module("model"), args.model)
+    model = model_cls(
+        num_classes=num_classes
     )
 
-    # 모델을 정의합니다. (학습한 모델이 있다면 torch.load로 모델을 불러주세요!)
-    device = torch.device('cuda')
-    model = ModelList.parse_model(model_name)(xdim=[3,512,384], ksize=3, cdims=[], hdims=[256], ydim=18).to(device)
-    print(model)
-    #model.init_param()
-    model.load_state_dict(torch.load(load_file_path))
+    # tarpath = os.path.join(saved_model, 'best.tar.gz')
+    # tar = tarfile.open(tarpath, 'r:gz')
+    # tar.extractall(path=saved_model)
+
+    model_path = os.path.join(saved_model, 'best.pth')
+    model.load_state_dict(torch.load(model_path, map_location=device))
+
+    return model
+
+
+@torch.no_grad()
+def inference(data_dir, model_dir, output_dir, args):
+    """
+    """
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    num_classes = MaskBaseDataset.num_classes  # 18
+    model = load_model(model_dir, num_classes, device).to(device)
     model.eval()
 
-    # 모델이 테스트 데이터셋을 예측하고 결과를 저장합니다.
-    all_predictions = []
-    for images in loader:
-        with torch.no_grad():
+    img_root = os.path.join(data_dir, 'images')
+    info_path = os.path.join(data_dir, 'info.csv')
+    info = pd.read_csv(info_path)
+
+    img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+    dataset = TestDataset(img_paths, args.resize)
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        num_workers=8,
+        shuffle=False,
+        pin_memory=use_cuda,
+        drop_last=False,
+    )
+
+    print("Calculating inference results..")
+    preds = []
+    with torch.no_grad():
+        for idx, images in enumerate(loader):
             images = images.to(device)
             pred = model(images)
             pred = pred.argmax(dim=-1)
-            all_predictions.extend(pred.cpu().numpy())
-    submission['ans'] = all_predictions
+            preds.extend(pred.cpu().numpy())
 
-    # 제출할 파일을 저장합니다.
-    submission.to_csv(os.path.join(test_dir, 'submission.csv'), index=False)
-    print('test inference is done!')
+    info['ans'] = preds
+    info.to_csv(os.path.join(output_dir, f'output.csv'), index=False)
+    print(f'Inference Done!')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Data and model checkpoints directories
+    parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--resize', type=tuple, default=(224, 224), help='resize size for image when you trained (default: (224, 224))')
+    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+
+    # Container environment
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_CHANNEL_MODEL', './model'))
+    parser.add_argument('--output_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR', './output'))
+
+    args = parser.parse_args()
+
+    data_dir = args.data_dir
+    model_dir = args.model_dir
+    output_dir = args.output_dir
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    inference(data_dir, model_dir, output_dir, args)
