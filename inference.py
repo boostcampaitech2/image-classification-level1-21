fe9f55007065887cc40c2bc642e952a02e85b7a2
lkm2835 <lkm2835@gmail.com>
2021-09-03 12:24:36 +0900

    remove f1loss, label smoothing, etc/

--- inference.py
+++ inference.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     # Data and model checkpoints directories
     parser.add_argument('--batch_size', type=int, default=1, help='input batch size for validing (default: 1000)')
     parser.add_argument('--resize', type=tuple, default=(224, 224), help='resize size for image when you trained (default: (224, 224))')
-    parser.add_argument('--model', type=str, default='MainModel', help='model type (default: MainModel)')
+    parser.add_argument('--model', type=str, default='CustomEfficientNet', help='model type (default: CustomEfficientNet)')
 
     # Container environment
     parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
